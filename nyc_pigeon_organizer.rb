66dfb65b7f63493cf7adc6ad90384f1f007533c5
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-24 01:37:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
     type.each do |type2,name|
       name.each do |names|
         if pigeon_list.keys.include?(names)
-          pigeon_list[names][attribute] = type2
+          pigeon_list[names] = {attribute => type2}
       pigeon_list[names] = Hash.new
     end
   end
